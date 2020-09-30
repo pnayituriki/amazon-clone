48eb1cf7996d04237491e273dd8556cb47dbce6c
Patrick NAYITURIKI <pnayituriki@gmail.com>
2020-09-30 23:55:36 +0200

    Setting Router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import React from "react";
 import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
-  return <div className="App"> Welcome</div>;
+  return (
+    <Router>
+      <div className="app">
+        <Switch>
+          <Route path="/login">
+            <h1>Login Page</h1>
+          </Route>
+          <Route path="/">
+            <h1>Home Page</h1>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
 }
 
 export default App;

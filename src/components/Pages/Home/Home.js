126bcfbbc562289c10189959c985285b6ff7ed9a
Patrick NAYITURIKI <pnayituriki@gmail.com>
2020-10-07 16:42:01 +0200

    Complete Home Banner

--- src/components/Pages/Home/Home.js
+++ src/components/Pages/Home/Home.js
@@ -1,11 +1,16 @@
 import React from "react";
+import "./Home.css";
 import Header from "../../Shared/Navbar/Header";
 
 function Home() {
   return (
-    <div>
+    <div className="home">
       <Header />
-      <h1>Home Page</h1>
+      <img
+        className="home_image"
+        src="https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2020/May/Hero/Fuji_TallHero_Computers_1x._CB432469755_.jpg"
+        alt=""
+      />
     </div>
   );
 }
